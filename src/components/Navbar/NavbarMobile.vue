ab3373a46f75df501743f99b932d4423cad17b00
Viktor Lovgren <lovgren.viktor@gmail.com>
2021-06-01 00:43:58 +0700

    404 page

--- src/components/Navbar/NavbarMobile.vue
+++ src/components/Navbar/NavbarMobile.vue
@@ -1,6 +1,6 @@
 <template>
   <nav
-    class="block md:hidden flex items-center justify-between flex-wrap container mx-auto py-4 dark:text-gray-400"
+    class="block md:hidden flex items-center justify-between flex-wrap container mx-auto py-2 dark:text-gray-400"
   >
     <div class="block flex-grow flex items-center w-auto mx-4">
       <div class="flex items-center flex-shrink-0 mr-6">
---
+++ src/pages/404.vue
@@ -0,0 +1,19 @@
+<template>
+<Layout>
+<div class="404-container flex flex-col-reverse md:flex-row justify-end md:justify-center text-gray-800 dark:text-gray-500 md:h-screen">
+    <div class="flex items-center justify-center mx-5">
+        <div class="flex-col items-center my-4">
+        <div>
+            <p class="text-center my-4 md:text-2xl">Sorry, nothing here!</p>
+            </div>
+            <div class="flex flex-center">
+            <a href="/" class="text-center mx-0 mx-auto bg-gray-500 hover:bg-black text-white font-bold py-2 px-4 rounded-full">Home</a>
+       </div>
+        </div>
+    </div>
+    <div class="flex items-center justify-center">
+        <g-image src="~/assets/images/404.png" class="md:h-3/4 w-auto"/>
+    </div>
+</div>
+</Layout>
+</template>
